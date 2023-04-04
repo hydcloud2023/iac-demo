380db70e89cc3b4d7b16f0cf0022e5beed37701f
hydcloud2023 <129813094+hydcloud2023@users.noreply.github.com>
2023-04-04 08:40:42 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAQAUQZ3DMYPC63XJ6"
-  secret_key = "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"
+  access_key = "AKIA4WCYQX3P427BWC7U"
+  secret_key = "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"
 }
